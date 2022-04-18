0422e6ec54569a650b02b3e49efa4d19cff86bca
Luiz Claudio <lcluizclaudio96@gmail.com>
2022-04-18 17:23:35 -0300

    fix name

--- src/controller/AuthenticateUserController.ts
+++ src/controller/AuthenticateUserController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { AuthenticateUserService } from "../services/AuthenticateUsedrService";
+import { AuthenticateUserService } from "../services/AuthenticateUserService";
 
 class AuthenticateUserController {
   async handle(request: Request, response: Response) {
