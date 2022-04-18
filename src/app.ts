c9fec7de5fc8bf4deb99277e0fbbfee094fce214
Luiz Claudio <lcluizclaudio96@gmail.com>
2022-04-18 17:22:04 -0300

    auth

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,20 @@
+import "dotenv/config";
 import express from "express";
+import { router } from "./routes";
 const app = express();
 const port = 4000;
+
+app.use(express.json());
+app.use(router);
+
+app.get("/github", (req, res) => {
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  );
+});
+app.get("/signin/callback", (req, res) => {
+  const { code } = req.query;
+  return res.json(code);
+});
+
 app.listen(port, () => console.log(`🔥 Server is running on PORT ${port}`));
